141a93825ebeae03281537acf799bfcb9155a30f
Глеб Усольцев <glebus008@gmail.com>
2025-11-24 19:26:44 +0500

    Add project files.

---
+++ Tyuiu.UsoltsevGP.Sprint4.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.UsoltsevGP.Sprint4.Task0.V17.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint4.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+            int res = ds.GetSumEvenArrEl(numsArray);
+            int wait = 34;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
