6e700ea94ab3da5582277820df3504e58b6e6d0d
Глеб Усольцев <glebus008@gmail.com>
2025-11-25 18:02:15 +0500

    Task1

---
+++ Tyuiu.UsoltsevGP.Sprint4.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.UsoltsevGP.Sprint4.Task1.V28.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint4.Task1.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 2, 4, 4, 5, 3, 4, 4, 6, 2, 4, 5, 5, 4, 4 };
+            int res = ds.Calculate(numsArray);
+            int wait = 375;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
